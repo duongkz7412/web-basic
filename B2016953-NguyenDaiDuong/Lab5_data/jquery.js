157784cbec9e6ed27d16b5849eaa970727963708
Nguyen Dai Duong <49199409+duongkz7412@users.noreply.github.com>
2022-12-11 13:13:41 +0700

    Delete B2016953-NguyenDaiDuong/Lab5_data directory

--- B2016953-NguyenDaiDuong/Lab5_data/jquery.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('body').addClass('loaded');
-
-
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-})
-});
